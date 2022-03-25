0056257517a93b0b2513f3e7f07b2ed76e3de277
ali <A.besharati@hotmail.com>
2022-03-25 18:50:30 +0430

    Add Validator Project

---
+++ Infrastructure/Validators/CustomerValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using Models.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validators
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(c => c.Email).EmailAddress(EmailValidationMode.AspNetCoreCompatible);
+
+        }
+    }
+}
---
+++ Infrastructure/Validators/OrderCompletedValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using Models.Events;
+
+namespace Validators
+{
+    public class OrderCompletedValidator : AbstractValidator<OrderCompleted>
+    {
+        public OrderCompletedValidator()
+        {
+            RuleFor(c => c.Email).EmailAddress(EmailValidationMode.AspNetCoreCompatible);
+        }
+    }
+}
