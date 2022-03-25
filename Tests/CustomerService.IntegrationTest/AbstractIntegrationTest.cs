43e2e61f0fcc1e1cea6226139d9302e7eca34f79
ali <A.besharati@hotmail.com>
2022-03-25 23:20:32 +0430

    Add   CustomerControllerTest

---
+++ Tests/CustomerService.IntegrationTest/AbstractIntegrationTest.cs
@@ -0,0 +1,18 @@
+﻿using AggregateGateway;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+
+namespace CustomerService.IntegrationTest
+{
+    public abstract class AbstractIntegrationTest
+    {
+
+
+        protected readonly HttpClient TestClient;
+        public AbstractIntegrationTest()
+        {
+            var appfactory = new WebApplicationFactory<Startup>();
+            TestClient = appfactory.CreateClient();
+        }
+    }
+}
---
+++ Tests/CustomerService.IntegrationTest/CustomerControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CustomerService.IntegrationTest
+{
+    public class CustomerControllerTest : AbstractIntegrationTest
+    {
+        [Fact]
+        public async Task Get_WithOut_Parameter_Should_Response_Badrequest()
+        {
+
+            var response = await TestClient.GetAsync("Customer");
+
+            var errormesage = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal("[\"'Email' must not be empty.\"]", errormesage);
+        }
+
+
+        [Fact]
+        public async Task Get_With_Wrong_Email_Should_Response_Badrequest()
+        {
+
+            var response = await TestClient.GetAsync("Customer?Email=test");
+
+            var errormesage = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal("[\"'Email' is not a valid email address.\"]", errormesage);
+        }
+
+
+
+
+    }
+}
