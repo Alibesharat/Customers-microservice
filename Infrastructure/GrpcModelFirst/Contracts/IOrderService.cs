2713c8995f737429f8b8f818ab59e1938bad5293
ali <A.besharati@hotmail.com>
2022-03-25 13:03:23 +0430

    Add Atterbute for grpc recognize

--- Infrastructure/GrpcModelFirst/Contracts/IOrderService.cs
+++ Infrastructure/GrpcModelFirst/Contracts/IOrderService.cs
@@ -1,10 +1,13 @@
 ﻿using GrpcModelFirst.Models;
+using System.ServiceModel;
 
 namespace GrpcModelFirst
 {
+    [ServiceContract]
+
     public interface IOrderService
     {
-
+        [OperationContract]
         void OrderComplete(OrderCompleteRequestDto dto);
     }
 }
