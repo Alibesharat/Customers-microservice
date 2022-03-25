53b128412df665265dbd3157f874be479130c5e8
ali <A.besharati@hotmail.com>
2022-03-25 11:17:22 +0430

    OrderComplete

--- Services/OrderServiceApp/Impelimentions/OrderService.cs
+++ Services/OrderServiceApp/Impelimentions/OrderService.cs
@@ -1,5 +1,8 @@
-﻿using GrpcModelFirst;
+﻿using DAL;
+using Entites;
+using GrpcModelFirst;
 using GrpcModelFirst.Models;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
@@ -7,9 +10,35 @@ namespace OrderServiceApp.Impelimentions
 {
     public class OrderService : IOrderService
     {
-        public Task<OrderCompleteResultDto> OrderComplete(OrderCompleteRequestDto dto)
+        readonly IStoreService _StoreService;
+        readonly ILogger<OrderService> _logger;
+
+        public OrderService(IStoreService storeService, ILogger<OrderService> logger)
         {
-            throw new NotImplementedException();
+            _StoreService = storeService;
+            _logger = logger;
+        }
+
+
+
+        public async Task<OrderCompleteResultDto> OrderComplete(OrderCompleteRequestDto dto)
+        {
+            var result = new OrderCompleteResultDto();
+            try
+            {
+                var customer = await _StoreService.FetchAsync<Customer>(dto.Email);
+                customer.PurchasedAt= DateTime.Now;
+                await _StoreService.AppendAsync(dto.Email, customer);
+                result.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+
+                result.IsSuccess = false;
+                result.Message = "Order not Complete See The logs";
+                _logger.LogError(ex, "Customer not set PurchasedAt  in storeDb");
+            }
+            return result;
         }
     }
 }
