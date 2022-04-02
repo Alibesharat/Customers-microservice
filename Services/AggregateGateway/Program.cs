dd7b696fa1229867b2d3098fa219dcad2dcc2d43
ali <A.besharati@hotmail.com>
2022-04-02 21:53:35 +0430

    Remove force Application to use 5000 port

--- Services/AggregateGateway/Program.cs
+++ Services/AggregateGateway/Program.cs
@@ -15,7 +15,6 @@ namespace AggregateGateway
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://0.0.0.0:5000");
                 });
     }
 }
